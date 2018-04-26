4a839d17e9107c964f38164ad774a22c71543831
WON SEOB SEO <won.seob.seo@zalando.fi>
2018-04-26 15:06:14 +0300

    Update lein kibit

--- profiles.clj
+++ profiles.clj
@@ -2,7 +2,7 @@
                    [jonase/eastwood "0.2.5"]
                    [lein-ancient "0.6.14"]
                    [lein-bikeshed "0.5.1"]
-                   [lein-kibit "0.0.8"]
+                   [lein-kibit "0.1.6-beta2"]
                    [lein-midje "3.2.1"]
                    [lein-nsorg "0.1.3"]
                    [lein-pprint "1.1.1"]

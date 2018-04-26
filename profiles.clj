a2cf6ed4135274cdc4f6fa49a4ffbe1ff65eecde
WON SEOB SEO <won.seob.seo@zalando.fi>
2018-04-26 10:25:59 +0300

    Initial commit

---
+++ profiles.clj
@@ -0,0 +1,12 @@
+{:user {:plugins  [[alembic "0.3.2"]
+                   [jonase/eastwood "0.2.5"]
+                   [lein-ancient "0.6.14"]
+                   [lein-bikeshed "0.5.1"]
+                   [lein-kibit "0.0.8"]
+                   [lein-midje "3.2.1"]
+                   [lein-nsorg "0.1.3"]
+                   [lein-pprint "1.1.1"]
+                   [venantius/yagni "0.1.4"]
+                   ]
+        :bikeshed {:max-line-length 120
+                   :docstrings      false}}}

d41e84b1e82fd2665f6188b7efecdee85cc9328b
WON SEOB SEO <wontheone1@users.noreply.github.com>
2019-05-09 20:48:54 +0300

    Update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins  [[alembic "0.3.2"]
+                   [cider/cider-nrepl "0.21.1"]
                    [jonase/eastwood "0.2.5"]
                    [lein-ancient "0.6.15"]
                    [lein-bikeshed "0.5.1"]
@@ -8,5 +9,10 @@
                    [lein-pprint "1.1.1"]
                    [venantius/yagni "0.1.4"]
                    ]
+        :dependencies [
+                       [slamhound "1.3.1"]
+                       [org.clojure/tools.nrepl "0.2.13"]
+                       [org.clojure/tools.trace "0.7.10"]
+                      ]
         :bikeshed {:max-line-length 120
                    :docstrings      false}}}
